2a92ee4ced6b05c16b4d81f0ebdb3c84c3d7dfcf
Jason Lang <jason.lang@shield.ai>
2023-10-15 22:49:31 -0400

    initial working version

--- rust_src/lib.rs
+++ rust_src/lib.rs
@@ -1,14 +1,74 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+
+// env constants
+#[allow(dead_code)]
+enum Actions {
+    NoAction = 0, 
+    MoveLeft = 1,
+    MoveRight = 2, 
+    MoveDown = 3, 
+    MoveUp = 4,
+}
+
+#[allow(dead_code)]
+enum Observations {
+    SelfVelX = 0, 
+    SelfVelY = 1, 
+    SelfPosX = 2,
+    SelfPosY = 3,
+    LandmarkOneRelPositionX = 4,
+    LandmarkOneRelPositionY = 5,
+    LandmarkTwoRelPositionX = 6,
+    LandmarkTwoRelPositionY = 7,
+    LandmarkThreeRelPositionX = 8,
+    LandmarkThreeRelPositionY = 9,
+    OtherAgentOneRelPositionX = 10,
+    OtherAgentOneRelPositionY = 11,
+    OtherAgentTwoRelPositionX = 12,
+    OtherAgentTwoRelPositionY = 13,
+    CommunicationOne = 14,
+    CommunicationTwo = 15,
+    CommunicationThree = 16,
+    CommunicationFour = 17,
+}
+
+#[pyfunction]
+fn towards_landmark(_py: Python, observations: Vec<f64>, target: i32) -> PyResult<i32> {
+    // turn the target assignment into the actual target location
+    let offset: i32 = target * 2;
+    let x_rel: f64 = observations[(Observations::LandmarkOneRelPositionX as i32 + offset) as usize];
+    let y_rel: f64 = observations[(Observations::LandmarkOneRelPositionY as i32 + offset) as usize];
+    let x_rel_abs: f64 = x_rel.abs();
+    let y_rel_abs: f64 = y_rel.abs();
+
+    // figure out if we are further from the target in the x or y direction
+    // and which direction we should actually go
+    if x_rel_abs < y_rel_abs {
+        if y_rel > 0.0 {
+            Ok(Actions::MoveUp as i32)
+        } else {
+            Ok(Actions::MoveDown as i32)
+        }
+    } else {
+        if x_rel > 0.0 {
+            Ok(Actions::MoveRight as i32)
+        } else {
+            Ok(Actions::MoveLeft as i32)
+        }
+    }
+}
+
 #[pyfunction]
 fn return_one() -> PyResult<i32> {
     return Ok(1)
 }
 
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn magent_autonomy(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(return_one, m)?)?;
+    m.add_function(wrap_pyfunction!(towards_landmark, m)?)?;
     Ok(())
 }
--- scripts/run_episode.py
+++ scripts/run_episode.py
@@ -1,20 +1,24 @@
 from pettingzoo import mpe
-from magent_autonomy import return_one
+from magent_autonomy import towards_landmark
+import time
 
 def setup_env():
-    return mpe.simple_spread_v3.parallel_env(N=3, local_ratio=0.5, max_cycles=50)
+    return mpe.simple_spread_v3.parallel_env(N=3, local_ratio=0.0, max_cycles=50, render_mode="human")
 
 if __name__ == "__main__":
     env = setup_env()
     observations, infos = env.reset()
 
     while env.agents:
-
-        actions = {agent: return_one() for agent in env.agents}
-        #print(actions)
-
+        # assemble actions for each agent
+        actions = {}
+        for agent in env.agents:
+            # TODO: add target assignment based on distance to targets
+            actions[agent] = towards_landmark(
+                observations[agent].tolist(), int(agent[-1])
+            )
         observations, rewards, terminations, truncations, infos = env.step(actions)
-
-        #print(observations)
+        # print(rewards)
+        # time.sleep(1)
 
     env.close()
